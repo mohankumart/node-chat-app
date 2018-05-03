0c812ac8167181d330ccb109ddeb7974056f053f
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-05-03 19:13:57 +0530

    added support for socket.io

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+var socket = io()
+socket.on('connect',()=>{
+    console.log('connected to server')
+    socket.emit('createEmail', {
+        to: 'hello@fireeye.com',
+        text: 'Hello World'
+    })
+})
+
+socket.on('disconnect',()=>{
+    console.log('disconnected to server')
+})
+
+socket.on('newMessage', (data)=>{
+    console.log(`New Message has come. ${data.from}`);
+})
+
+socket.emit('createMessage', {
+    'from': 'Adrew',
+    'text': 'hello andrew'
+})
--- server/server.js
+++ server/server.js
@@ -1,13 +1,37 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const publicPath = path.join(__dirname, '../public')
 
 const app = express()
+const server = http.createServer(app)
+var io = socketIO(server)
+
+io.on('connection',(socket)=>{
+    console.log('connection accepted')
+
+    socket.emit('newMessage', {
+        from: 'mohan@fireeye.com',
+        text: 'hello',
+        creatAt: 'today'
+    })
+
+    socket.on('createMessage', (message) =>{
+        console.log(`createMessage ${message}`)
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log('socket client dsiconnected')
+    })    
+    
+})
+
 
 app.use(express.static(publicPath))
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`server started on port ${port}`)
 })
