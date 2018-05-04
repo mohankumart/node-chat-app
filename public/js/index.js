dd08434fb6bb60e077369e49a0a0dac6c43c980a
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-05-04 22:52:40 +0530

    ui changes

--- public/js/index.js
+++ public/js/index.js
@@ -14,5 +14,18 @@ socket.on('disconnect',()=>{
 
 socket.on('newMessage', (data)=>{
     console.log(`${data.text} from ${data.from}`);
+    var li = jQuery('<li></li>')
+    li.text(`${data.from}: ${data.text}`)
+    jQuery('#messages').append(li)
 })
 
+
+jQuery('#message-form').on('submit', (e)=>{
+    e.preventDefault()
+    socket.emit('createMessage',{
+        from: "User",
+        text: jQuery('[name=message]').val()
+    },(data)=>{
+        
+    })
+})
--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ io.on('connection',(socket)=>{
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user is joined'))
 
-    socket.on('createMessage', (message) =>{
-        console.log(`createMessage ${message}`)
+    socket.on('createMessage', (message, callback) =>{
+        console.log(`createMessage ${message.text}`)
         io.emit('newMessage', generateMessage(message.from, message.text))
+        callback('Server recived message')
     })
 
     socket.on('disconnect', ()=>{
